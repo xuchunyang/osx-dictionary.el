7ca311c53dc82190ba9aa6ae03e5bfe6cfb71db7
Chunyang Xu <xuchunyang56@gmail.com>
2014-12-16 17:01:26 +0800

    Add basic implementation
    Many code is borrowed directly from sdcv.el and ydcv.el

--- dictionary.el
+++ dictionary.el
@@ -19,38 +19,70 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+;;
+;; Interface for Dictionary.app
+;;
+;; Translation word by Dictionary.app, and display translation use
+;; buffer.
+;;
+;; Before are commands you can use:
+;; `dictionary-search-word'
+;; Search word from input via mini buffer
+;; `dictionary-search-pointer'
+;; Search word under pointer (cursor)
+;;
+;; Tips:
+;;
+;; If current mark is active, dictionary commands will translate
+;; region string, otherwise translate word around point.
+;;
 
-;; Example usage:
+;;; Installation:
+;;
+;; Put dictionary.el to your `load-path'.
+;; And the following to your Emacs initialization file
+;;
+;; (require 'dictionary)
+;; Example key binding
+;; (global-set-key (kbd "C-c d") 'dictionary-search-pointer)
+
 
 ;;; Code:
 
-(setq dictionary-mode-font-lock-Keywords
-      '(;; Word class
-        ("noun\\|adjective\\|det\\|verb\\|adverb\\|abbreviation\\|preposition\\|suffix\\|prefix\\|conjunction\\|symb" . font-lock-type-face)
-        ;; Serial number
-        ("^[0-9]+" . font-lock-builtin-face)
-        ;; Dictionary comment
-        ("DERIVATIVES\\|ORIGIN\\|PHRASES" . font-lock-comment-face)))
+(defvar dictionary-mode-font-lock-Keywords
+  '(
+    ;; TODO: 1. add more keywords like "名" and "代". 2. adopt more precise regex
+    ;; Word class
+    ("noun\\|adjective\\|det\\|verb\\|adverb\\|abbreviation\\|preposition\\|suffix\\|prefix\\|conjunction\\|symb" . font-lock-type-face)
+    ;; Serial number
+    ("^[0-9]+" . font-lock-builtin-face)
+    ;; Dictionary comment
+    ("DERIVATIVES\\|ORIGIN\\|PHRASES" . font-lock-comment-face))
+  "Keywords to highlight in `dictionary-mode'.")
 
-(setq dictionary-mode-map
-      (let ((map (make-sparse-keymap)))
-        ;; Dictionary command
-        (define-key map "q" 'dictionary-quit)
-        (define-key map "i" 'dictionary-search-word)
-        ;; Isearch
-        (define-key map "S" 'isearch-forward-regexp)
-        (define-key map "R" 'isearch-backward-regexp)
-        (define-key map "s" 'isearch-forward)
-        (define-key map "r" 'isearch-backward)
-        ;; Misc.
-        (define-key map "DEL" 'scroll-down)
-        (define-key map " " 'scroll-up)
-        (define-key map "l" 'forward-char)
-        (define-key map "h" 'backward-char)
-        (define-key map "?" 'describe-mode)
-        map))
+(defvar dictionary-mode-map
+  (let ((map (make-sparse-keymap)))
+    ;; Dictionary command
+    (define-key map "q" 'dictionary-quit)
+    (define-key map "i" 'dictionary-search-word)
+    ;; Isearch
+    (define-key map "S" 'isearch-forward-regexp)
+    (define-key map "R" 'isearch-backward-regexp)
+    (define-key map "s" 'isearch-forward)
+    (define-key map "r" 'isearch-backward)
+    ;; Misc.
+    (define-key map "DEL" 'scroll-down)
+    (define-key map " " 'scroll-up)
+    (define-key map "l" 'forward-char)
+    (define-key map "h" 'backward-char)
+    (define-key map "?" 'describe-mode)
+    map)
+  "Keymap for `dictionary-mode'.")
 
 (defconst dictionary-buffer-name "*Dictionary*")
+
+(defvar dictionary-previous-window-configuration nil
+  "Window configuration before switching to dictionary buffer.")
 
 (define-derived-mode dictionary-mode fundamental-mode
   (setq font-lock-defaults '(dictionary-mode-font-lock-Keywords))
@@ -58,8 +90,14 @@
   (message "dictionary-mode: init"))
 
 (defun dictionary-quit ()
+  "Quit dictionary: reselect previously selected buffer."
   (interactive)
-  (bury-buffer))
+  (if (window-configuration-p dictionary-previous-window-configuration)
+      (progn
+        (set-window-configuration dictionary-previous-window-configuration)
+        (setq dictionary-previous-window-configuration nil)
+        (bury-buffer (dictionary-get-buffer)))
+    (bury-buffer)))
 
 (defun dictionary-get-buffer ()
   "Get the dictionary buffer.  Create one if there's none."
@@ -86,7 +124,7 @@
   "Prompt for input WORD.
 And show translation in other buffer."
   (interactive)
-  (let ((word (read-from-minibuffer "Enter word: ")))
+  (let ((word (dictionary-prompt-input)))
     (with-current-buffer (get-buffer-create dictionary-buffer-name)
       (setq buffer-read-only nil)
       (erase-buffer)
@@ -95,4 +133,35 @@ And show translation in other buffer."
       (goto-char (point-min))
       (setq buffer-read-only t))))
 
+(defun dictionary-search-pointer ()
+  "Get current word.
+And display complete translations in other buffer."
+  (interactive)
+  (let ((word (dictionary-region-or-word)))
+    (with-current-buffer (get-buffer-create dictionary-buffer-name)
+      (setq buffer-read-only nil)
+      (erase-buffer)
+      (insert (dictionary-search word))
+      (dictionary-goto-dictionary)
+      (goto-char (point-min))
+      (setq buffer-read-only t))))
+
+
+
+(defun dictionary-prompt-input ()
+  "Prompt input object for translate."
+  (read-string (format "Word (%s): " (or (dictionary-region-or-word) ""))
+               nil nil
+               (dictionary-region-or-word)))
+
+(defun dictionary-region-or-word ()
+  "Return region or word around point.
+If `mark-active' on, return region string.
+Otherwise return word around point."
+  (if mark-active
+      (buffer-substring-no-properties (region-beginning)
+                                      (region-end))
+    (thing-at-point 'word)))
+
+(provide 'dictionary)
 ;;; dictionary.el ends here
