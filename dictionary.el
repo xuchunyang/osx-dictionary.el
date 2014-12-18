81cd0a562f096f3a4186d6a17a7309acff720f51
Chunyang Xu <xuchunyang56@gmail.com>
2014-12-18 12:06:03 +0800

    Add misc comment

--- dictionary.el
+++ dictionary.el
@@ -4,6 +4,8 @@
 
 ;; Author: Chunyang Xu <xuchunyang56@gmail.com>
 ;; URL: https://github.com/xuchunyang/dictionary.el
+;; Version: 0.1
+;; keywords: help, dictionary, tools
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
