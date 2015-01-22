74f86681b2c43da7e94015c59ff4bfeb87369b68
Chunyang Xu <xuchunyang56@gmail.com>
2015-01-22 21:38:45 +0800

    Format raw result by adding  back old code

--- osx-dictionary.m
+++ osx-dictionary.m
@@ -45,8 +45,10 @@ Options:\n\
 int main(int argc, char *argv[]) {
   char *dict_name = NULL;
   char *word = NULL;
-  NSArray *dicts = NULL;
-  NSDictionary *s_names = NULL;;
+  int word_len = 0;
+  NSArray *dicts;
+  NSDictionary *s_names;
+  NSString *result;
   int c;
 
   opterr = 0;
@@ -70,6 +72,7 @@ int main(int argc, char *argv[]) {
     }
 
   word = argv[optind];
+  word_len = strlen(word);
   if (dict_name && word) {
     NSString *dictionaryName = @(dict_name);
     NSString *ns_word = @(word);
@@ -117,16 +120,178 @@ int main(int argc, char *argv[]) {
       return -1;
     } else {
       printf("dict=%s, word=%s\n", dict_name, word);
-      NSLog(@"%@\n", defStr);
+      result = defStr;
     }
   } else if (word) {            // Use Default Dictionary
     NSString* ns_word = [NSString stringWithUTF8String:word];
-    NSLog(@"%@", (NSString*)DCSCopyTextDefinition(NULL, (CFStringRef)ns_word,
-                                                  CFRangeMake(0,[ns_word length])));
+    result = (NSString*)DCSCopyTextDefinition(NULL, (CFStringRef)ns_word, CFRangeMake(0,[ns_word length]));
   } else {
     usage();
     return -1;
   }
+
+  char* r = (char*)[result UTF8String];
+  int len = strlen(r);
+  if (len < 1) return 0;
+  char s[len * 2];
+  int i, j;
+  char nr1[] = { -30, -106, -72 };
+  char nr2[] = { -30, -106, -74 };
+  char nr3[] = { -30, -128, -94 };
+  char nr4[] = { -17, -67, -98, -52, -127 };
+  char nr5[] = { -17, -67, -98, -52, -128 };
+  char paren1[] = { -29, -128, -106 };
+  char paren2[] = { -29, -128, -105 };
+  char paren3[] = { -29, -128, -104 };
+  int num = 0, newnum = 0;
+  char al = 'a';
+  char C = 0, U = 0, slash = 0;
+  char paren = 1;
+  char firstparen = 1;
+  i = j = 0;
+  if (word_len + 9 < len) {
+    char flg = 1;
+    for (i = 0; i < word_len; ++i) {
+      if (tolower(r[i]) != tolower(word[i])) {
+        flg = 0; break;
+      }
+    }
+    if (flg) {
+      if (r[i] == '.' || strncmp(r + i, paren3, 3) == 0) {
+        for (i = 0; i < word_len + (r[i] == '.'); ++i)
+          s[j++] = r[i];
+        s[j++] = '\n';
+      } else {
+        i = j = 0;
+      }
+    } else {
+      i = j = 0;
+    }
+  }
+  for ( ; i < len; ++i, ++j) {
+    if (strncmp(r + i, nr1, 3) == 0 || strncmp(r + i, nr3, 3) == 0) {
+      if (j && s[j - 1] == '\n') --j;
+      else s[j] = '\n';
+      s[++j] = ' ';
+      s[++j] = ' ';
+      s[++j] = r[i];
+      s[++j] = r[++i];
+      s[++j] = r[++i];
+    } else if (strncmp(r + i, nr2, 3) == 0) {
+      s[j] = '\n';
+      i += 2;
+    } else if (strncmp(r + i, nr4, 5) == 0 || strncmp(r + i, nr5, 5) == 0) {
+      s[j] = '\n';
+      s[++j] = ' ';
+      s[++j] = ' ';
+      s[++j] = r[i];
+      s[++j] = r[++i];
+      s[++j] = r[++i];
+      s[++j] = r[++i];
+      s[++j] = r[++i];
+    } else if (i + 3 < len && (r[i] == -30 && r[i + 1] == -111 && -97 < r[i + 2] && r[i + 2] < -76)) {
+      s[j] = '\n';
+      s[++j] = ' ';
+      s[++j] = r[i];
+      s[++j] = r[++i];
+      s[++j] = r[++i];
+    } else if (i + 3 < len && !isalpha(r[i]) && isalpha(r[i + 1]) && r[i + 2] == '.') {
+      if (r[i + 1] == al + 1 || r[i + 1] == 'a' || r[i + 1] == 'A') {
+        s[j] = r[i];
+        s[++j] = '\n';
+        s[++j] = ' ';
+        al = s[++j] = r[++i];
+        s[++j] = r[++i];
+      } else {
+        s[j] = r[i];
+      }
+    } else if (strncmp(r + i, "DERIVATIVES", 11) == 0 ||
+               strncmp(r + i, "PHRASES", 7) == 0 ||
+               strncmp(r + i, "ORIGIN", 6) == 0) {
+      s[j] = '\n';
+      s[++j] = r[i];
+    } else if (i + 3 < len && isdigit(r[i]) && isdigit(r[i + 1]) && r[i + 2] == ' ' && 0 < i && !isdigit(r[i - 1])) {
+      newnum = (r[i] - '0') * 10 + (r[i + 1] - '0');
+      if (0 < newnum && (num < newnum || newnum < 2) && newnum <= num + 2) {
+        if (j > 1 && s[j - 1] != '\n') {
+          s[j] = '\n';
+          s[++j] = r[i];
+          s[++j] = r[++i];
+        } else {
+          s[j] = r[i];
+        }
+        num = newnum;
+        if (i + 3 < len && (r[i + 2] == 'C' || r[i + 2] == 'U')) {
+          s[++j] = r[++i];
+          C = U = 1;
+          while ((C && r[i + 1] == 'C') || (U && r[i + 1] == 'U')) {
+            s[++j] = r[++i];
+            if (r[i] == 'C') C = 0;
+            if (r[i] == 'U') U = 0;
+          }
+          s[++j] = ' ';
+        }
+      } else {
+        s[j] = r[i];
+      }
+    } else if (r[i] == '/') {
+      if (slash == 1 && i + 1 < len && r[i + 1] != '\n' && firstparen != 2) {
+        s[j] = r[i];
+        s[++j] = '\n';
+      } else {
+        s[j] = r[i];
+      }
+      slash++;
+    } else if (i + 2 < len && isdigit(r[i]) && r[i + 1] == ' ' && 0 < i && !isdigit(r[i - 1])) {
+      newnum = r[i] - '0';
+      if (0 < newnum && (num < newnum || newnum < 2) && newnum <= num + 2) {
+        if (j > 1 && s[j - 1] != '\n') {
+          s[j] = '\n';
+          s[++j] = r[i];
+        } else {
+          s[j] = r[i];
+        }
+        num = newnum;
+        if (i + 3 < len && (r[i + 2] == 'C' || r[i + 2] == 'U')) {
+          s[++j] = r[++i];
+          C = U = 1;
+          while ((C && r[i + 1] == 'C') || (U && r[i + 1] == 'U')) {
+            s[++j] = r[++i];
+            if (r[i] == 'C') C = 0;
+            if (r[i] == 'U') U = 0;
+          }
+          s[++j] = ' ';
+        }
+      } else {
+        s[j] = r[i];
+      }
+    } else if (!num && paren && strncmp(r + i, paren1, 3) == 0 && j > 1 && s[j - 1] != '\n') {
+      s[j] = '\n';
+      s[++j] = r[i];
+      s[++j] = r[++i];
+      s[++j] = r[++i];
+    } else if (!num && paren && strncmp(r + i, paren2, 3) == 0) {
+      s[j] = r[i];
+      s[++j] = r[++i];
+      s[++j] = r[++i];
+      s[++j] = '\n';
+      paren = 0;
+    } else if (firstparen == 1 && r[i] == '(') {
+      s[j] = r[i];
+      firstparen = 2;
+    } else if (firstparen == 2 && r[i] == ')') {
+      s[j] = r[i];
+      if (i + 1 < len && r[i + 1] == ' ') {
+        s[++j] = '\n';
+        ++i;
+      }
+      firstparen = 0;
+    } else {
+      s[j] = r[i];
+    }
+  }
+  s[j] = '\0';
+  printf("%s", s);
 
   return 0;
 }
