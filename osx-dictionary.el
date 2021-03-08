953456711c5bc2fdfcd853eb6cae018867ebc066
Xu Chunyang <xuchunyang56@gmail.com>
2021-03-08 14:32:22 +0800

    [#25] Fix string-replace

--- osx-dictionary.el
+++ osx-dictionary.el
@@ -139,7 +139,7 @@ Turning on Text mode runs the normal hook `osx-dictionary-mode-hook'."
 (defun osx-dictionary--get-current-word ()
   (save-excursion
     (goto-char (point-min))
-    (string-replace "·" "" (thing-at-point 'word))))
+    (replace-regexp-in-string (rx "·") "" (thing-at-point 'word))))
 
 (defun osx-dictionary-quit ()
   "Quit osx-dictionary: reselect previously selected buffer."
